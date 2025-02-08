744a74a7c8c3d5cc3789ed169c1b42112d8b079c
Mariaximena <mximena07@gmail.com>
2025-02-08 16:50:31 -0500

    se agregó el banner principal con todos los estilos

--- src/principalBanner.jsx
+++ src/principalBanner.jsx
@@ -3,9 +3,13 @@
 export function PrincipalBanner() {
     return (
         <section className="principal-banner">
-            <div>
-                <h2 className="banner-title">TRANSFORMA TU <br/>CELULAR EN UN PUNTO DE <br/>VENTA</h2>
-                <h3>Cobra a tus clientes sin importar donde estés con nuestro lector de tarjetas de Mercado Pago y usando tu smartphone o table.</h3>
+            <div className="section-banner">
+                <h2 className="banner-title">TRANSFORMA TU <br/>CELULAR EN UN PUNTO DE <br/> VENTA</h2>
+                <h3 className="banner-text">Cobra a tus clientes sin importar donde estés con nuestro lector de tarjetas de Mercado Pago y usando tu smartphone o table.</h3>
+                <label className="label-text"> Ingresa tu email
+                    <input type="email" className="banner-input-1"></input>
+                </label>
+                <button className="banner-button">Quiero probarlo</button>
             </div>
         </section>
    
