e85d72050c8c3f929678f4ed5637e527e854031b
Mariaximena <mximena07@gmail.com>
2025-02-08 14:33:33 -0500

    se agregó la sección completa de NavBar con estilos
    # Conflicts:
    #	README.md

---
+++ src/principalBanner.jsx
@@ -0,0 +1,13 @@
+
+
+export function PrincipalBanner() {
+    return (
+        <section className="principal-banner">
+            <div>
+                <h2 className="banner-title">TRANSFORMA TU <br/>CELULAR EN UN PUNTO DE <br/>VENTA</h2>
+                <h3>Cobra a tus clientes sin importar donde estés con nuestro lector de tarjetas de Mercado Pago y usando tu smartphone o table.</h3>
+            </div>
+        </section>
+   
+    )
+}
